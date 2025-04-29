df9bf87dbb5264307f8e26ba7757e5c0b7ad8f9d
cloudman4416 <134954873+cloudman4416@users.noreply.github.com>
2025-04-30 01:42:36 +0200

    Create Workspace.Mobs.Bosses.Akaza.Npc_Configuration.lua

---
+++ Project_Slayer/Workspace.Mobs.Bosses.Akaza.Npc_Configuration.lua
@@ -0,0 +1,106 @@
+-- Decompiler will be improved VERY SOON!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/wyButjTMhM)
+-- Decompiled on 2025-04-29 16:42:03
+-- Luau version 6, Types version 3
+-- Time taken: 0.012839 seconds
+
+collection_service = game:GetService("CollectionService") -- Setting global
+game.ReplicatedStorage:WaitForChild("Modules")
+game.ReplicatedStorage.Modules:WaitForChild("Npc_Attacks")
+local tick_upvr = tick
+local module_upvr = {
+	Npc = nil;
+	Name = "Akeza";
+	Code = "Akaza";
+	Quest_Return = "Akeza_Killed";
+	Heal = {
+		Heal_Every = 1;
+		CoolDown_From_Stun = 30;
+		Last_Healed = 0;
+		Add_Amount = 0.01;
+	};
+	Npc_Spawning = {
+		Spawn_Time = 900;
+		Last_Spawned = tick_upvr();
+		Despawn_Time = 2;
+		Spawned_At = Vector3.new();
+		Max_Distance = 90;
+		Already_Firstly_Detected_Someone = false;
+		Spawn_Locations = {Vector3.new(2020.34, 553.500, -124.49)};
+	};
+	Following = {
+		Following_Id = "";
+		Player = nil;
+		Let_Go_Distance = 90;
+		Capture_Distance = 20;
+		Max_Npc_Per_Player = 5;
+		Follow_Type = 0;
+		Lost_Goal = nil;
+		Run_Away = {
+			Run_HP = 0;
+			Ranges = {
+				Max_Range = 37;
+				Min_Range = 19;
+				Safe_Range = 24;
+			};
+			Start_Running_Range = 0;
+			Look_At_Player_When_No_Walk = false;
+			Look_At_When_walking_away = false;
+		};
+	};
+	Clashing = {
+		Last = 0;
+		CoolDown = 0.14;
+		Randomness = {1, 3};
+	};
+	On_Idle = {
+		Move_Places = false;
+		Goal_Point = nil;
+		last_arrived = 0;
+		delay_before_next_departure = {2, 6};
+		Move_To_Locations = {Vector3.new(-175.47, 1.5, -159.25), Vector3.new(-105.48, 0.5, -111.21), Vector3.new(-107.66, 0.5, -221.89)};
+	};
+	Defaults = {
+		WalkSpeed = 16;
+		JumpPower = 50;
+	};
+	Run = {
+		Run_Speed = 25;
+		Is_Running = false;
+		Run_On_Idle = {
+			Distance = 20;
+			Away_Or_Near = "Away";
+			Enabled = false;
+		};
+		Run_On_Lost = {
+			Distance = 30;
+			Away_Or_Near = "Away";
+			Enabled = false;
+		};
+		Run_On_Following = {
+			Distance = 13;
+			Away_Or_Near = "Away";
+			Enabled = true;
+		};
+		Run_On_Running_Away = {
+			Distance = 70;
+			Away_Or_Near = "Near";
+			Enabled = true;
+		};
+	};
+	Attacks = {
+		Last_Performed = 0;
+		Perform_CoolDown = 0.5;
+		Holding = nil;
+		Settings = {
+			Last_Stunned = 0;
+			Hold_Moves_Disabled = false;
+			None_Hold_Moves_Disabled = false;
+			Last_Punched = 0;
+			id = "";
+		};
+	};
+	Functions_Debree = {};
+	Debris = {};
+}
+return module_upvr
