cebcf841a8d10147b214f9d5aa049021e8e870ae
cloudman4416 <134954873+cloudman4416@users.noreply.github.com>
2025-04-30 00:24:57 +0200

    Create ReplicatedStorage.Modules.Server.Skills_Modules_Handler.lua

---
+++ Project_Slayer/ReplicatedStorage.Modules.Server.Skills_Modules_Handler.lua
@@ -0,0 +1,712 @@
+-- Decompiler will be improved VERY SOON!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/wyButjTMhM)
+-- Decompiled on 2025-04-29 15:23:48
+-- Luau version 6, Types version 3
+-- Time taken: 0.013465 seconds
+
+local module_upvr = {
+	Skills = {
+		Water_Surface_Slash = {
+			Race = {1, 2, 4};
+			Power = "Water";
+			Mastery = "Water";
+			MasteryNeed = 0;
+			addiframefor = 0.3;
+		};
+		Water_fall_basin = {
+			Race = {1, 2, 4};
+			Power = "Water";
+			Mastery = "Water";
+			MasteryNeed = 0;
+			addiframefor = 0.7;
+		};
+		Tamari2_power_kick = {
+			Race = {3, 4};
+			Art = "Tamari";
+			Mastery = "Tamari";
+			MasteryNeed = 23;
+		};
+		Constent_flux_asd123 = {
+			Race = {1, 2, 4};
+			Power = "Water";
+			Mastery = "Water";
+			MasteryNeed = 46;
+		};
+		Piercing_rain_drop = {
+			Race = {1, 2, 4};
+			Power = "Water";
+			Mastery = "Water";
+			MasteryNeed = 28;
+		};
+		Water_Dragon = {
+			Race = {1, 2, 4};
+			Power = "Water";
+			Mastery = "Water";
+			MasteryNeed = 23;
+		};
+		Water_wheel = {
+			Race = {1, 2, 4};
+			Power = "Water";
+			Mastery = "Water";
+			MasteryNeed = 17;
+		};
+		ThunderClapAndFlash = {
+			Race = {1, 2, 4};
+			Power = "Thunder";
+			Mastery = "Thunder";
+			MasteryNeed = 0;
+		};
+		ThunderClapAndFlashsixfold = {
+			Race = {1, 2, 4};
+			Power = "Thunder";
+			Mastery = "Thunder";
+			MasteryNeed = 45;
+		};
+		HeatLightning = {
+			Race = {1, 2, 4};
+			Power = "Thunder";
+			Mastery = "Thunder";
+			MasteryNeed = 16;
+		};
+		Thunderbreathingrapidslashes = {
+			Race = {1, 2, 4};
+			Power = "Thunder";
+			Mastery = "Thunder";
+			MasteryNeed = 20;
+		};
+		thunderbreathingricespirit = {
+			Race = {1, 2, 4};
+			Power = "Thunder";
+			Mastery = "Thunder";
+			MasteryNeed = 30;
+		};
+		thunderbreathingthunderdragon = {
+			Race = {1, 2, 4};
+			Power = "Thunder";
+			Mastery = "Thunder";
+			MasteryNeed = 40;
+		};
+		thunder_rumblinmg_thunder_bolkt_skill = {
+			Race = {1, 2, 4};
+			Power = "Thunder";
+			Mastery = "Thunder";
+			MasteryNeed = 26;
+			ignoreinvisibhead = 1;
+		};
+		arrow_knock_back = {
+			Race = {3, 4};
+			Art = "Arrow";
+			Mastery = "Arrow";
+			MasteryNeed = 0;
+		};
+		Piercing_Arrow = {
+			Race = {3, 4};
+			Art = "Arrow";
+			Mastery = "Arrow";
+			MasteryNeed = 22;
+		};
+		Koketsu_Arrow = {
+			Race = {3, 4};
+			Art = "Arrow";
+			Mastery = "Arrow";
+			MasteryNeed = 31;
+		};
+		["Arrow_Spikes_//Thing"] = {
+			Race = {3, 4};
+			Art = "Arrow";
+			Mastery = "Arrow";
+			MasteryNeed = 43;
+		};
+		Tamari2_Normal_Throw = {
+			Race = {3, 4};
+			Art = "Tamari";
+			Mastery = "Tamari";
+		};
+		Tamari2_double_Throw = {
+			Race = {3, 4};
+			Art = "Tamari";
+			Mastery = "Tamari";
+			MasteryNeed = 10;
+		};
+		Tamari_meteor_thing = {
+			Race = {3, 4};
+			Art = "Tamari";
+			Mastery = "Tamari";
+			MasteryNeed = 48;
+		};
+		Tamari2_multiuple_air_thijngs = {
+			Race = {3, 4};
+			Art = "Tamari";
+			Mastery = "Tamari";
+			MasteryNeed = 37;
+		};
+		["Claws//Slash"] = {
+			Race = {1, 2, 3, 4};
+			Mastery = "Claws";
+			MasteryNeed = 10;
+			Item = "ClawsIsEquiped";
+		};
+		["Claws//Spin"] = {
+			Race = {1, 2, 3, 4};
+			Mastery = "Claws";
+			MasteryNeed = 20;
+			Item = "ClawsIsEquiped";
+		};
+		Insect_breathing_jaw_breaker = {
+			Race = {1, 2, 4};
+			Power = "Insect";
+			Mastery = "Insect";
+			MasteryNeed = 0;
+		};
+		Insect_Breathing_True_Flatter = {
+			Race = {1, 2, 4};
+			Power = "Insect";
+			Mastery = "Insect";
+			MasteryNeed = 17;
+		};
+		Insect_breathing_compound_eye_hexagon = {
+			Race = {1, 2, 4};
+			Power = "Insect";
+			Mastery = "Insect";
+			MasteryNeed = 23;
+		};
+		insect_breathing_dance_of_the_centipede = {
+			Race = {1, 2, 4};
+			Power = "Insect";
+			Mastery = "Insect";
+			MasteryNeed = 28;
+			addiframefor = 0.2;
+		};
+		insect_breathing_mentis_kick = {
+			Race = {1, 2, 4};
+			Power = "Insect";
+			Mastery = "Insect";
+			MasteryNeed = 35;
+			createvalue = "insect_mantis_counter123";
+		};
+		insect_breathing_caprice = {
+			Race = {1, 2, 4};
+			Power = "Insect";
+			Mastery = "Insect";
+			MasteryNeed = 46;
+		};
+		blood_burst_blood_shot = {
+			Race = {3, 4};
+			Art = "Blood";
+			Mastery = "Blood Burst";
+			MasteryNeed = 0;
+		};
+		blood_burst_explosive_choke_slam = {
+			Race = {3, 4};
+			Art = "Blood";
+			Mastery = "Blood Burst";
+			MasteryNeed = 20;
+			addiframefor = 0.35;
+		};
+		blood_burst_explosive_burst = {
+			Race = {3, 4};
+			Art = "Blood";
+			Mastery = "Blood Burst";
+			MasteryNeed = 25;
+			addiframefor = 0;
+		};
+		blood_burst_explosive_land_mines = {
+			Race = {3, 4};
+			Art = "Blood";
+			Mastery = "Blood Burst";
+			MasteryNeed = 30;
+			addiframefor = 0;
+		};
+		explosive_blood_fury_explosion_Demon_art = {
+			Race = {3, 4};
+			Art = "Blood";
+			Mastery = "Blood Burst";
+			MasteryNeed = 37;
+			addiframefor = 0;
+		};
+		explosive_demon_art_blood_burst = {
+			Race = {3, 4};
+			Art = "Blood";
+			Mastery = "Blood Burst";
+			MasteryNeed = 45;
+			addiframefor = 0.3;
+		};
+		wind_breathing_purifying_wind = {
+			Race = {1, 2, 4};
+			Power = "Wind";
+			Mastery = "Wind";
+			MasteryNeed = 0;
+		};
+		Wind_breathing_whirlwind_cutter = {
+			Race = {1, 2, 4};
+			Power = "Wind";
+			Mastery = "Wind";
+			MasteryNeed = 15;
+		};
+		Wind_breathing_clean_storm_tree = {
+			Race = {1, 2, 4};
+			Power = "Wind";
+			Mastery = "Wind";
+			MasteryNeed = 22;
+		};
+		wind_breathing_cold_mountain_wind = {
+			Race = {1, 2, 4};
+			Power = "Wind";
+			Mastery = "Wind";
+			MasteryNeed = 37;
+		};
+		Wind_breathing_black_wind_mountain_mist = {
+			Race = {1, 2, 4};
+			Power = "Wind";
+			Mastery = "Wind";
+			MasteryNeed = 30;
+			addiframefor = 0.35;
+		};
+		Wind_breathing_idaten_typhoon = {
+			Race = {1, 2, 4};
+			Power = "Wind";
+			Mastery = "Wind";
+			MasteryNeed = 50;
+		};
+		reaperbda_reapofdispair = {
+			Race = {3, 4};
+			Art = "Reaper";
+			Mastery = "Reaper";
+			MasteryNeed = 0;
+		};
+		reaper_demon_art_blazing_amputation = {
+			Race = {3, 4};
+			Art = "Reaper";
+			Mastery = "Reaper";
+			MasteryNeed = 23;
+		};
+		reaper_demon_art_quick_tackle = {
+			Race = {3, 4};
+			Art = "Reaper";
+			Mastery = "Reaper";
+			MasteryNeed = 35;
+		};
+		Reaper_demon_art_runasd123 = {
+			Race = {3, 4};
+			Art = "Reaper";
+			Mastery = "Reaper";
+			MasteryNeed = 45;
+		};
+		clan_skill_indominate_will = {
+			Race = {1, 2, 3, 4};
+		};
+		heal_thing_asd123 = {
+			Race = {1, 2, 3, 4};
+			Clan = "Kamado";
+		};
+		Tamari_Arms = {
+			Race = {3, 4};
+			Art = "Tamari";
+			Mastery = "Tamari";
+			MasteryNeed = 30;
+		};
+		god_speed = {
+			Race = {1, 2, 4};
+		};
+		Arrow_Flight = {
+			Race = {3, 4};
+			Art = "Arrow";
+			Mastery = "Arrow";
+			MasteryNeed = 11;
+		};
+		reaper_demon_art_sonido = {
+			Race = {3, 4};
+			Art = "Reaper";
+			Mastery = "Reaper";
+			MasteryNeed = 15;
+			invisibility = 6;
+		};
+		flame_breathing_rising_scorching_sun = {
+			Race = {1, 2, 4};
+			Power = "Flame";
+			Mastery = "Flame";
+			MasteryNeed = 0;
+		};
+		flame_breathing_unknowing_fire = {
+			Race = {1, 2, 4};
+			Power = "Flame";
+			Mastery = "Flame";
+			MasteryNeed = 0;
+		};
+		flame_breathing_blooming_undulation = {
+			Race = {1, 2, 4};
+			Power = "Flame";
+			Mastery = "Flame";
+			MasteryNeed = 0;
+		};
+		flame_breathing_flaming_eruption = {
+			Race = {1, 2, 4};
+			Power = "Flame";
+			Mastery = "Flame";
+			MasteryNeed = 0;
+			addiframefor = 0.6;
+		};
+		flame_flaming_tinger_skill = {
+			Race = {1, 2, 4};
+			Power = "Flame";
+			Mastery = "Flame";
+			MasteryNeed = 0;
+		};
+		flame_breathing_purgatory = {
+			Race = {1, 2, 4};
+			Power = "Flame";
+			Mastery = "Flame";
+			MasteryNeed = 0;
+		};
+		Scythe_triple_slash = {
+			Race = {1, 2, 3, 4};
+			Mastery = "Scythe";
+			MasteryNeed = 0;
+			Item = "ScytheIsEquiped";
+		};
+		scythe_asteroid_reap = {
+			Race = {1, 2, 3, 4};
+			Mastery = "Scythe";
+			MasteryNeed = 0;
+			addiframefor = 0.6;
+			Item = "ScytheIsEquiped";
+		};
+		beast_breathing_pierce = {
+			Race = {1, 2, 4};
+			Power = "Beast";
+			Mastery = "Beast";
+			MasteryNeed = 0;
+		};
+		beast_breathing_crazy_cutting = {
+			Race = {1, 2, 4};
+			Power = "Beast";
+			Mastery = "Beast";
+			MasteryNeed = 0;
+		};
+		beast_breathing_Bending_slash = {
+			Race = {1, 2, 4};
+			Power = "Beast";
+			Mastery = "Beast";
+			MasteryNeed = 0;
+		};
+		Beast_breathing_throuwing_strike = {
+			Race = {1, 2, 4};
+			Power = "Beast";
+			Mastery = "Beast";
+			MasteryNeed = 0;
+		};
+		Beast_breathing_devouring_slash = {
+			Race = {1, 2, 4};
+			Power = "Beast";
+			Mastery = "Beast";
+			MasteryNeed = 0;
+			addiframefor = 0.75;
+		};
+		Beast_breathing_devouring_rush = {
+			Race = {1, 2, 4};
+			Power = "Beast";
+			Mastery = "Beast";
+			MasteryNeed = 0;
+		};
+		akaza_bda_air_type = {
+			Race = {3, 4};
+			Art = "Shockwave";
+			Mastery = "Shockwave";
+			MasteryNeed = 0;
+		};
+		akaza_bda_chaotic_type = {
+			Race = {3, 4};
+			Art = "Shockwave";
+			Mastery = "Shockwave";
+			MasteryNeed = 0;
+		};
+		Akaza_crown_split_skill = {
+			Race = {3, 4};
+			Art = "Shockwave";
+			Mastery = "Shockwave";
+			MasteryNeed = 0;
+		};
+		akaza_bda_explosion_Furry = {
+			Race = {3, 4};
+			Art = "Shockwave";
+			Mastery = "Shockwave";
+			MasteryNeed = 0;
+		};
+		akaza_flashing_williow_skillasd = {
+			Race = {3, 4};
+			Art = "Shockwave";
+			Mastery = "Shockwave";
+			MasteryNeed = 0;
+			addiframefor = 0.65;
+		};
+		akaza_bda_annihilation_type = {
+			Race = {3, 4};
+			Art = "Shockwave";
+			Mastery = "Shockwave";
+			MasteryNeed = 0;
+		};
+		akaza_bda_compass_needle = {
+			Race = {3, 4};
+			Art = "Shockwave";
+			Mastery = "Shockwave";
+			MasteryNeed = 0;
+			addiframe2for = 3;
+		};
+		dream_man_hypnosis = {
+			Race = {3, 4};
+			Art = "Dream";
+			Mastery = "Dream";
+			MasteryNeed = 0;
+		};
+		Dream_Bda_Melodic_Whisper = {
+			Race = {3, 4};
+			Art = "Dream";
+			Mastery = "Dream";
+			MasteryNeed = 0;
+		};
+		dream_bda_echoeing_whisper = {
+			Race = {3, 4};
+			Art = "Dream";
+			Mastery = "Dream";
+			MasteryNeed = 0;
+		};
+		dream_bda_piercing_flesh = {
+			Race = {3, 4};
+			Art = "Dream";
+			Mastery = "Dream";
+			MasteryNeed = 0;
+		};
+		dream_bda_spiritual_core = {
+			Race = {3, 4};
+			Art = "Dream";
+			Mastery = "Dream";
+			MasteryNeed = 0;
+		};
+		dream_bda_flesh_monster = {
+			Race = {3, 4};
+			Art = "Dream";
+			Mastery = "Dream";
+			MasteryNeed = 0;
+			addiframefor = 3;
+		};
+		swampbda_swamp_puddle = {
+			Race = {3, 4};
+			Art = "Swamp";
+			Mastery = "Swamp";
+			MasteryNeed = 0;
+			invisibility = 6;
+		};
+		swamp_bda_traveling_claws = {
+			Race = {3, 4};
+			Art = "Swamp";
+			Mastery = "Swamp";
+			MasteryNeed = 0;
+		};
+		swamp_bda_swamp_eject = {
+			Race = {3, 4};
+			Art = "Swamp";
+			Mastery = "Swamp";
+			MasteryNeed = 0;
+			invisibility = 6;
+		};
+		Swamp_trap_swamp_bda = {
+			Race = {3, 4};
+			Art = "Swamp";
+			Mastery = "Swamp";
+			MasteryNeed = 0;
+		};
+		Swamp_bda_self_replication = {
+			Race = {3, 4};
+			Art = "Swamp";
+			Mastery = "Swamp";
+			MasteryNeed = 0;
+		};
+		swamp_bda_swamp_domain = {
+			Race = {3, 4};
+			Art = "Swamp";
+			Mastery = "Swamp";
+			MasteryNeed = 0;
+			addiframefor = 2;
+		};
+		mist_breathing_lunar_dispersing_mist = {
+			Race = {1, 2, 4};
+			Power = "Mist";
+			Mastery = "Mist";
+			MasteryNeed = 0;
+		};
+		mist_breathing_cloud_and_haze = {
+			Race = {1, 2, 4};
+			Power = "Mist";
+			Mastery = "Mist";
+			MasteryNeed = 0;
+		};
+		mist_breathing_eight_layerd_Dispersing_mist = {
+			Race = {1, 2, 4};
+			Power = "Mist";
+			Mastery = "Mist";
+			MasteryNeed = 0;
+		};
+		mist_breathing_distant_haze = {
+			Race = {1, 2, 4};
+			Power = "Mist";
+			Mastery = "Mist";
+			MasteryNeed = 0;
+		};
+		mist_breathing_shifting_flow_flash = {
+			Race = {1, 2, 4};
+			Power = "Mist";
+			Mastery = "Mist";
+			MasteryNeed = 0;
+		};
+		mist_breathing_obscuring_clouds = {
+			Race = {1, 2, 4};
+			Power = "Mist";
+			Mastery = "Mist";
+			MasteryNeed = 0;
+			invisibility = 10;
+		};
+		heart_ablaze_mode = {
+			Race = {1, 2, 4};
+		};
+		clan_skill_immense_reflexes = {
+			Race = {1, 2, 3, 4};
+		};
+		clan_skill_reflexe_thing123asd = {
+			Race = {1, 2, 3, 4};
+		};
+		clan_skill_furiosity = {
+			Race = {1, 2, 3, 4};
+		};
+		clan_skill_final_resort = {
+			Race = {1, 2, 3, 4};
+		};
+		clan_skill_soothing_voice = {
+			Race = {1, 2, 3, 4};
+		};
+		snow_breatihng_layers_frost = {
+			Race = {1, 2, 4};
+			Power = "Snow";
+			Mastery = "Snow";
+			MasteryNeed = 0;
+			addiframefor = 0.6;
+		};
+		snow_breathing_frost_path = {
+			Race = {1, 2, 4};
+			Power = "Snow";
+			Mastery = "Snow";
+			MasteryNeed = 0;
+			invisibility = 6;
+		};
+		snow_breathing_frozen_desert = {
+			Race = {1, 2, 4};
+			Power = "Snow";
+			Mastery = "Snow";
+			MasteryNeed = 0;
+		};
+		snow_breathing_frost_gnaw = {
+			Race = {1, 2, 4};
+			Power = "Snow";
+			Mastery = "Snow";
+			MasteryNeed = 0;
+		};
+		snow_breathing_illusory_Storm = {
+			Race = {1, 2, 4};
+			Power = "Snow";
+			Mastery = "Snow";
+			MasteryNeed = 0;
+			addiframefor = 1;
+		};
+		snow_breathing_snowtide_vortex = {
+			Race = {1, 2, 4};
+			Power = "Snow";
+			Mastery = "Snow";
+			MasteryNeed = 0;
+		};
+		War_fans_war_tornado_skill = {
+			Race = {3, 4};
+			MasteryNeed = 0;
+			Item = "WarFansEquipped";
+		};
+		War_fans_war_Drums = {
+			Race = {3, 4};
+			MasteryNeed = 0;
+			Item = "WarFansEquipped";
+		};
+		sound_breathing_bursting_boom = {
+			Race = {1, 2, 4};
+			Power = "Sound";
+			Mastery = "Sound";
+			MasteryNeed = 0;
+		};
+		Sound_breathing_roar = {
+			Race = {1, 2, 4};
+			Power = "Sound";
+			Mastery = "Sound";
+			MasteryNeed = 0;
+		};
+		sound_breathing_resounding_slashes = {
+			Race = {1, 2, 4};
+			Power = "Sound";
+			Mastery = "Sound";
+			MasteryNeed = 0;
+		};
+		sound_breathing_explosive_impact = {
+			Race = {1, 2, 4};
+			Power = "Sound";
+			Mastery = "Sound";
+			MasteryNeed = 0;
+		};
+		sound_breathing_smoke_screen = {
+			Race = {1, 2, 4};
+			Power = "Sound";
+			Mastery = "Sound";
+			MasteryNeed = 0;
+			invisibility = 4.5;
+			addiframefor = 1;
+		};
+		sound_breathing_string_performance = {
+			Race = {1, 2, 4};
+			Power = "Sound";
+			Mastery = "Sound";
+			MasteryNeed = 0;
+		};
+		ice_demon_art_wintry_iciles = {
+			Race = {3, 4};
+			Art = "Ice";
+			Mastery = "Ice";
+			MasteryNeed = 0;
+		};
+		ice_demon_art_cold_white_princess = {
+			Race = {3, 4};
+			Art = "Ice";
+			Mastery = "Ice";
+			MasteryNeed = 0;
+		};
+		ice_demon_art_barren_hanging_garden = {
+			Race = {3, 4};
+			Art = "Ice";
+			Mastery = "Ice";
+			MasteryNeed = 0;
+		};
+		ice_demon_art_freezing_cloud = {
+			Race = {3, 4};
+			Art = "Ice";
+			Mastery = "Ice";
+			MasteryNeed = 0;
+		};
+		ice_demon_art_lotus_vines = {
+			Race = {3, 4};
+			Art = "Ice";
+			Mastery = "Ice";
+			MasteryNeed = 0;
+		};
+		ice_demon_art_bodhisatva = {
+			Race = {3, 4};
+			Art = "Ice";
+			Mastery = "Ice";
+			MasteryNeed = 0;
+			addiframefor = 4.5;
+		};
+	};
+}
+return module_upvr
